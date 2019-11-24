421dfddf7a0deeb729153df1c0adc30467742143
dmitriy.litichevskiy <litichevskiydv@gmail.com>
2019-11-24 17:10:59 +0300

    Error in default logger was fixed

--- src/logging/defaultLogger.js
+++ src/logging/defaultLogger.js
@@ -1,17 +1,9 @@
-const log = (level, message, payload) =>
-  console.log(
-    JSON.stringify(
-      {
-        level,
-        message,
-        payload
-      },
-      (key, value) => (value instanceof Error ? { message: value.message, stack: value.stack } : value)
-    )
-  );
+const { serializeError } = require("serialize-error");
+
+const log = (level, message, payload) => console.log(JSON.stringify(serializeError({ level, message, payload })));
 
 const defaultLogger = {};
 const defaultLevels = ["fatal", "error", "warn", "info", "debug"];
-defaultLevels.forEach(level => (defaultLogger[level] = (message, payload) => log(level, message, payload | {})));
+defaultLevels.forEach(level => (defaultLogger[level] = (message, payload) => log(level, message, payload || {})));
 
 module.exports = defaultLogger;
